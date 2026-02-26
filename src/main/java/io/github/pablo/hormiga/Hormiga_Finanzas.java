eaa87d70f15e9dd5460ad43b8ffdd539c8b8d4e2
Pablo Andres Canelos Canelos <pabloccanelos.contacto@gmail.com>
2026-02-26 09:48:59 -0300

    Primer commit desde netbeans

---
+++ src/main/java/io/github/pablo/hormiga/Hormiga_Finanzas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package io.github.pablo.hormiga;
+
+/**
+ *
+ * @author Pavilion X360
+ */
+public class Hormiga_Finanzas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
